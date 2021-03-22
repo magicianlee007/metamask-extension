ae3a5a2474318967d51633312f56b960ca0622d0
Brad Decker <git@braddecker.dev>
2021-03-22 15:24:11 -0500

    make migration more robust

--- app/scripts/migrations/056.js
+++ app/scripts/migrations/056.js
@@ -1,4 +1,5 @@
 import { cloneDeep, keyBy } from 'lodash';
+import createId from '../../../shared/modules/random-id';
 
 const version = 56;
 
@@ -17,10 +18,26 @@ export default {
 };
 
 function transformState(state) {
-  if (state?.TransactionController?.transactions) {
+  if (
+    state?.TransactionController?.transactions &&
+    Array.isArray(state.TransactionController.transactions) &&
+    !state.TransactionController.transactions.some(
+      (item) =>
+        typeof item !== 'object' || typeof item.txParams === 'undefined',
+    )
+  ) {
     state.TransactionController.transactions = keyBy(
       state.TransactionController.transactions,
-      'id',
+      // In case for some reason any of a user's transactions do not have an id
+      // generate a new one for the transaction.
+      (tx) => {
+        if (typeof tx.id === 'undefined' || tx.id === null) {
+          // This mutates the item in the array, so will result in a change to
+          // the state.
+          tx.id = createId();
+        }
+        return tx.id;
+      },
     );
   }
   return state;
--- app/scripts/migrations/056.test.js
+++ app/scripts/migrations/056.test.js
@@ -67,6 +67,83 @@ describe('migration #56', function () {
     });
   });
 
+  it('should handle transactions without an id, just in case', async function () {
+    const oldStorage = {
+      meta: {},
+      data: {
+        TransactionController: {
+          transactions: [
+            {
+              id: 0,
+              txParams: { foo: 'bar' },
+            },
+            {
+              txParams: { foo: 'bar' },
+            },
+            {
+              txParams: { foo: 'bar' },
+            },
+            {
+              txParams: { foo: 'bar' },
+            },
+          ],
+        },
+        foo: 'bar',
+      },
+    };
+
+    const newStorage = await migration56.migrate(oldStorage);
+    const expectedTransactions = {};
+    for (const transaction of Object.values(
+      newStorage.data.TransactionController.transactions,
+    )) {
+      // Make sure each transaction now has an id.
+      assert.ok(
+        typeof transaction.id !== 'undefined',
+        'transaction id is undefined',
+      );
+      // Build expected transaction object
+      expectedTransactions[transaction.id] = transaction;
+    }
+    // Ensure that we got the correct number of transactions
+    assert.equal(
+      Object.keys(expectedTransactions).length,
+      oldStorage.data.TransactionController.transactions.length,
+    );
+    // Ensure that the one transaction with id is preserved, even though it is
+    // a falsy id.
+    assert.equal(newStorage.data.TransactionController.transactions[0].id, 0);
+  });
+
+  it('should not blow up if transactions are not an array', async function () {
+    const storageWithTransactionsAsString = {
+      meta: {},
+      data: {
+        TransactionController: {
+          transactions: 'someone might have weird state in the future',
+        },
+      },
+    };
+    const storageWithTransactionsAsArrayOfString = {
+      meta: {},
+      data: {
+        TransactionController: {
+          transactions: 'someone might have weird state in the future'.split(
+            '',
+          ),
+        },
+      },
+    };
+    const result1 = await migration56.migrate(storageWithTransactionsAsString);
+
+    const result2 = await migration56.migrate(
+      storageWithTransactionsAsArrayOfString,
+    );
+
+    assert.deepEqual(storageWithTransactionsAsString.data, result1.data);
+    assert.deepEqual(storageWithTransactionsAsArrayOfString.data, result2.data);
+  });
+
   it('should do nothing if transactions state does not exist', async function () {
     const oldStorage = {
       meta: {},
