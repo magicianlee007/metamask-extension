9d86a88971c0191b64189b839ff38cfa7144b4a1
magicianlee007 <magicianlee007@gmail.com>
2021-03-27 02:55:00 +0400

    refactor: change metadata

--- ui/app/components/ui/metafox-logo/metafox-logo.component.js
+++ ui/app/components/ui/metafox-logo/metafox-logo.component.js
@@ -34,7 +34,7 @@ export default class MetaFoxLogo extends PureComponent {
         />
         <img
           {...iconProps}
-          src="/images/logo/metamask-fox.svg"
+          src="/images/logo/blue-fox.png"
           className={classnames(
             'app-header__metafox-logo',
             'app-header__metafox-logo--icon',
